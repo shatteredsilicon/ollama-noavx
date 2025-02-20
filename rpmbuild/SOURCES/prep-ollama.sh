241c10b9505e859499926b4c75245ec7197fa978
Thien Nguyen <nthien86@gmail.com>
2025-02-21 03:46:08 +0700

    Build ollama-0.5.11 no-avx

--- rpmbuild/SOURCES/prep-ollama.sh
+++ rpmbuild/SOURCES/prep-ollama.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
+set -e
+
 export GOAMD64=v2
 
-rm -rf ollama ollama-0.5.7
-rm -f ollama-0.5.7-git.tar*
-rm -f ollama-0.5.7-vendor.tar*
+rm -rf ollama-0.5.11
+rm -f v0.5.11.tar.gz
+rm -f ollama-0.5.11-vendor.tar*
+rm -f ollama-0.5.11.tar*
 
-rm -f ollama-0.5.7.tar.gz
-wget -O ollama-0.5.7.tar.gz "https://github.com/ollama/ollama/archive/refs/tags/v0.5.7.tar.gz"
+wget https://github.com/ollama/ollama/archive/refs/tags/v0.5.11.tar.gz
+tar -zxvf v0.5.11.tar.gz
+tar -zcvf ollama-0.5.11.tar.gz ollama-0.5.11
 
-git clone https://github.com/ollama/ollama.git
-mv ollama ollama-0.5.7
-tar -cf ollama-0.5.7.tar ollama-0.5.7
-pushd ollama-0.5.7
-git checkout v0.5.7
-popd
-pushd ollama-0.5.7
-git submodule init
-git submodule update --force llama/llama.cpp
-patch < ../golang-version.patch
+cp golang-version.patch ollama-0.5.11/
+pushd ollama-0.5.11
+patch -p1 < golang-version.patch
 go mod vendor
-tar -cf ../ollama-0.5.7-git.tar .git*
-tar -cf ../ollama-0.5.7-vendor.tar vendor
+tar -zcvf ../ollama-0.5.11-vendor.tar.gz vendor
 popd
-rm -f ollama-0.5.7-git.tar.xz
-rm -f ollama-0.5.7-vendor.tar.xz
-pxz -9 ollama-0.5.7-git.tar
-pxz -9 ollama-0.5.7-vendor.tar
